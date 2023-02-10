9a6e8e8afadb81a5278f7633fb12c31ac57770cf
Shanekmm <124464343+Shanekmm@users.noreply.github.com>
2023-02-10 13:46:33 +0530

    Update web.java

--- web.java
+++ web.java
@@ -1,12 +1,18 @@
 package tesst;
-public class web {
+public class Web {
 	public static void main(String[] args)
 	{
 		String[] urls = {"apple.com", "google.com", "fb.com"};
-		browser chrome=new browser(urls);
-		browser firefox=new browser();
-		chrome.Set("insta.com");
-		chrome.prinths();
-		firefox.printhistory();
+		Browser chrome=new Browser(urls);
+		System.out.println("\n Visited urls in chrome \n");
+		chrome.printHistory();
+		String[] url = {"insta.com","twitter.com"};
+		chrome.set(url);
+		System.out.println("\n Updated urls in chrome \n");
+		chrome.printHistory();
+	    String[] ur = {"reddit.com","snapchat.com"};
+		Browser firefox=new Browser(ur);
+		System.out.println("\n New History of Firefox \n");
+		firefox.printHistory();
 	}
 }
