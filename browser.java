e83c4d4c7dc606dbf672f108153044d00f08bf55
Shanekmm <124464343+Shanekmm@users.noreply.github.com>
2023-02-10 13:45:46 +0530

    Update browser.java

--- browser.java
+++ browser.java
@@ -1,37 +1,24 @@
 package tesst;
 
-public class browser {
+public class Browser {
 	private static String[] history=new String[100];
 	private static int n;
-	public int no;
-	public String[] hs=new String[100];
-	public browser()
-	{
-		for(int i=0;i<n;i++) {
-			Set(history[i]);
-		}
-		
-	}
-	public browser(String[] url) {
-		for (int i = 0; i < url.length; i++) {
-            history[i] = url[i];
-            n++;
-			
+	public Browser() {}
+	public Browser(String[] url) {
+	    set(url);
+	      
         }		
+	public void set(String[] url) {
+		for(int i=0;i<url.length;i++)
+		history[n++] = url[i];	
 	}
-	public void Set(String url) {
-		hs[no++] = url;
-	}
-	public void prinths() {
-		for(int i=0;i<no;i++) {
-			System.out.println(hs[i]);
-		}
-	}
-	public void printhistory() {
+	public void printHistory() {
 		for(int i=0;i<n;i++) {
 			System.out.println(history[i]);
+			
 		}
 	}
+	
 }
 	
 
